ce05b4ef8d2833414f55ac3dbb2fc9970d04c40e
Kyhan Turner <kyhanpt@gmail.com>
2016-11-21 20:47:38 -0800

    create description routes for individual campsites

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var campgroundSchema = new mongoose.Schema({ 
 		name: String,
-		image: String
+		image: String,
+		description: String
 })
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -25,20 +26,33 @@ app.get('/campgrounds', function(req, res){
 		if(err) {
 			console.log(err);
 		} else {
-		res.render('campgrounds', {campgrounds: campgrounds});
+		res.render('index', {campgrounds: campgrounds});
 		}
 	})
 })
 
 app.get('/campgrounds/new', function(req, res) {
 	res.render('new');
+});
+
+app.get('/campgrounds/:id', function(req, res) {
+
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if(err){
+			console.log(err);
+		} else {
+			res.render('show', {campground: foundCampground});
+		}
+	})
+
 })
 
 app.post('/campgrounds', function(req, res){
 
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description};
 	
 	Campground.create(newCampground, function(err, newlyCreated) {
 		if(err) { 
